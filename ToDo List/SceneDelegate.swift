ee61c5179891a92df349f36d7af037278b794094
vasiliev-evgeny <vasiliev.creativnoemilo99@gmail.com>
2025-09-04 14:22:20 +0300

    Initial commit

--- ToDo List/SceneDelegate.swift
+++ ToDo List/SceneDelegate.swift
@@ -17,6 +17,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window: UIWindow = UIWindow(windowScene: windowScene)
+        let mainVC = ViewController()
+        let navController = UINavigationController(rootViewController: mainVC)
+        window.rootViewController = navController
+        self.window = window
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
